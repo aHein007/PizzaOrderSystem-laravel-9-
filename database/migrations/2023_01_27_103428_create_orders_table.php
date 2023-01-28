770ee09f4da61d67779195727fb139dee540f842
Aung Thu Hein <100287026+aHein007@users.noreply.github.com>
2023-01-28 09:04:04 +0630

    user_register_and_project_setup

---
+++ database/migrations/2023_01_27_103428_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('order_id');
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->integer('cart_id');
+            $table->integer('total-price');
+            $table->integer('status')->default(0); // 0->pending 1->success 2->reject
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
